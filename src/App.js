3ef8f2ebc1f7156c4f1018cfcd4378fc1bb9bf7c
omer.mindivanli <omer.mindivanli@gmail.com>
2018-07-17 15:34:23 +0200

    this.state and bind and onSubmit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { list } from '../node_modules/postcss';
 
 class App extends Component {
-  onClick() {
-    alert('omer mindivanli');
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Omer Mindivanli',
+      age: 24
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onSubmit(event) {
+    event.preventDefault();
+    console.log('this.input.value');
   }
   render() {
-    const title = 'Hej Hej!';
-    const newTitle = 'Such Nice Flowers!';
-    const arrList = [1, 2, 3, 'omer js'];
-    const obj = { name: 'niyazi', age: 21 };
     return (
       <div className="App">
-        <h1>{title}</h1>
-        <h2>{true ? newTitle : 'Such Nice Flowers!'}</h2>
-        <h3>{newTitle}</h3>
-        <h4>
-          {arrList.map(item => {
-            return item;
-          })}
-        </h4>
-        <h5>{obj.name + obj.age}</h5>
+        <h2>
+          Full Name: <br />
+          {this.state.title}
+        </h2>
+        <h3>His Age: {this.state.age}</h3>
+        <form onSubmit={this.onSubmit}>
+          <input onChange={this.onChange} />
+        </form>
       </div>
     );
   }
